52d13ee474f57cc7da28c3cc3c9d106ba3e647cd
Patrascanu Mihai <patrascanu.mihai@yahoo.com>
2022-09-04 23:52:27 +0100

    Development
    Creating User, Currency, Token models and controllers

---
+++ api/app/Http/Controllers/Api/V1/CurrencyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Currency;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+
+}
---
+++ api/app/Http/Controllers/Api/V1/TokenController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Token;
+use Illuminate\Http\Request;
+
+class TokenController extends Controller
+{
+
+}
